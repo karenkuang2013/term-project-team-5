834ca529f895bbaaadf221185c16cc8237cd4583
Felix Lee <Felix Lee>
2016-11-11 22:38:46 -0800

    Implemented chat component (chat.html) which can be included into any page that needs a chat. Updated chat component to only emit events to users on the same page.

--- routes/lobby.js
+++ routes/lobby.js
@@ -12,24 +12,27 @@ module.exports = function(io) {
     });
     
     io.on('connection', function(socket) {
-        console.log("A user connected to the /lobby namespace");
+        console.log("A user connected to /");
     
         socket.on('disconnect', function() {
-            console.log("user disconnected from /chat namespace");
+            console.log("user disconnected from /");
         });
     });
     
+    var io_lobby = io.of('lobby');
+    
+    io_lobby.on('connection', function(socket) {
+        console.log("A user connected to the /lobby namespace");
+    
+        socket.on('disconnect', function() {
+            console.log("user disconnected from /lobby namespace");
+        });
+        
+        socket.on('chat_sent', function(message) {
+            console.log("message: " + message);
+            io_lobby.emit('chat_received', "Socket id(" + socket.id + "): " + message);
+        });
+    }); 
+    
     return router;
 }
-/*
-var io_lobby = io.of('/chat');
-
-io_lobby.on('connection', function(socket) {
-    console.log("A user connected to the /chat namespace");
-    
-    socket.on('disconnect', function() {
-        console.log("user disconnected from /chat namespace");
-    });
-});
-*/
-//module.exports = router;
