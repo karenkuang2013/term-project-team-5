d428ed7bcfc04ae2f25ffa6fa34db6a0dd70d144
anuagg <anuagg0102@gmail.com>
2016-12-02 17:13:58 -0800

    config change

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ config.db_host = 'localhost'
 config.db_port = 5432
 config.db_name = 'rummydb'
 config.db_user = 'postgres'
-config.db_pass = 'QAZxsw_456'
+config.db_pass = 'postgres'
 
 module.exports = config
