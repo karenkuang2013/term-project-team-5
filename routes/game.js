f531c9b14fd9ec94641ef55cb800b87bc6c33240
Rajat Arora <rarora@mail.sfsu.edu>
2016-12-09 02:09:50 -0800

    Integrated game with modular chat

--- routes/game.js
+++ routes/game.js
@@ -3,43 +3,89 @@ module.exports = function(db, io) {
   const express = require('express');
   const router = express.Router();
   const path = require('path');
-  const rummy = require('./rummyserver');
-  
+  const gameServer = require('./gameserver')
+  const database = require('./database')(db)
+
   let gameId;
   let session;
   let username;
+  let playerId;
 
-  router.get('/', function(request, response){
-    session = request.session;
+  function init(req) {
+    session = req.session;
     username = session.user;
-    gameId = request.query.gameId;
-    
-    response.render('game', { USERNAME: username });
+    playerId = session.player_id;
+
+    gameServer.init(db, io)
+  }
+
+  router.get('/createGame', (req, resp) => {
+
+    init(req)
+    gameId = gameServer.createNewGame()
+    console.log(playerId + ' requested new game')
+    resp.redirect('/game/'+gameId)
   })
 
-  const game_io = io.of('/game');
-  game_io.on('connection', function(socket) {
-    socket.join(gameId); //check for undefined
-    console.log("A user connected to /game namespace, room " + gameId);
-    
-    socket.on('chat_sent', function(message){
-      username = message.substr(0, message.indexOf(' '));
-      message = message.substr(message.indexOf(' ')+1);
-      
-      game_io.to(gameId).emit('chat_received', username + ": " + message);
-    });
-    
-    socket.on('disconnect', function() {
-      console.log("user disconnected from /game namespace");
+  router.get('/joinGame/:gameId', (req, resp) => {
+
+    init(req)
+    gameId = req.params.gameId
+    gameServer.joinGame(gameId)
+    resp.redirect('/game/'+gameId)
+
+  })
+
+  router.get('/:gameId', (req, resp) => {
+
+    init(req)
+    gameId = req.params.gameId
+    resp.render('game', { USERNAME: username, gameId: gameId })
+
+    // console.log('/game/'+gameId);
+    const game_io = io.of('/game');
+
+    game_io.on('connection', function(socket) {
+
+      socket.on('player joined', (data) => {
+        socket.join(data.gameId.toString())
+        gameServer.checkGamePlayerCount(socket, gameId)
+      })
+
+      console.log("A user connected to /game namespace, room " + gameId);
+
+      socket.on('chat_sent', function(message){
+        username = message.substr(0, message.indexOf(' '));
+        message = message.substr(message.indexOf(' ')+1);
+
+        game_io.to(gameId).emit('chat_received', username + ": " + message);
+      });
+
+      socket.on('disconnect', function() {
+        console.log("user disconnected from /game namespace");
+      });
+
     });
 
-  });
-  
-  /*
-  io.on('connection', function(socket){
-    rummy.initGame (io, socket, db)
   })
-  */
+
+  // const game_io = io.of('/game');
+  // game_io.on('connection', function(socket) {
+  //   socket.join(gameId); //check for undefined
+  //   console.log("A user connected to /game namespace, room " + gameId);
+  //
+  //   socket.on('chat_sent', function(message){
+  //     username = message.substr(0, message.indexOf(' '));
+  //     message = message.substr(message.indexOf(' ')+1);
+  //
+  //     game_io.to(gameId).emit('chat_received', username + ": " + message);
+  //   });
+  //
+  //   socket.on('disconnect', function() {
+  //     console.log("user disconnected from /game namespace");
+  //   });
+  //
+  // });
 
   return router
 }
